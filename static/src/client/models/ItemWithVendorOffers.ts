b024495ca2ced7286f08ae56a6e4baca0adb70bc
tclesius <tomclesius@gmail.com>
2023-08-08 00:53:55 +0200

    client added

---
+++ static/src/client/models/ItemWithVendorOffers.ts
@@ -0,0 +1,14 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { VendorOfferBase } from './VendorOfferBase';
+
+export type ItemWithVendorOffers = {
+    market_hash_name: string;
+    classid: string;
+    name: string;
+    icon_url: string;
+    vendor_offers?: Array<VendorOfferBase>;
+};
---
+++ static/src/client/models/VendorOfferBase.ts
@@ -0,0 +1,12 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type VendorOfferBase = {
+    vendorid: number;
+    lowest_price: number;
+    median_price: number;
+    affiliate_link: string;
+    sell_listings: number;
+};
